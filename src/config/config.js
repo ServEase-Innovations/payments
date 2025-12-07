4ebd505a78810e882ffe07545f3c76dd7101908e
Ronit Kumar Maity <maity.ronit18@gmail.com>
2025-12-07 20:14:54 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -11,11 +11,11 @@ dotenv.config({
 export default {
   env,
   postgres: {
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.database,
-    port: process.env.port
+    host: process.env.POSTGRES_HOST || '127.0.0.1',
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  port: process.env.POSTGRES_PORT || 5432,
   },
   mongo: {
     uri: process.env.MONGO_URI
