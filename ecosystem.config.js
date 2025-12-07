ecacd873fb00f7f407b33f83df5d95fe60ebb762
Ronit Kumar Maity <maity.ronit18@gmail.com>
2025-12-07 17:47:50 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,17 @@ module.exports = {
   apps: [
     {
       name: "serveaso-dev",
-      script: "src/index.js",
+      script: "index.js",
       env_dev: {
         NODE_ENV: "development"
       },
       name: "serveaso-qa",
-      script: "src/index.js",
+      script: "index.js",
       env_qa: {
         NODE_ENV: "qa"
       },
       name: "serveaso-prod",
-      script: "src/index.js",
+      script: "index.js",
       env_prod: {
         NODE_ENV: "production"
       }
